c304de23cf978becce625a53cf41538a4e7adeaa
kimbugyeong <sa01023@naver.com>
2023-04-25 01:05:39 +0900

    [FIX] swagger수정

--- backend/src/main/java/com/dopamines/backend/test/controller/TestController.java
+++ backend/src/main/java/com/dopamines/backend/test/controller/TestController.java
@@ -17,14 +17,14 @@ import java.util.List;
 @RequestMapping("/test")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*", allowedHeaders = "*")
-@Api(value = "test", description = "테스트 컨트롤러에요!")
+@Api(value = "test", description = "테스트 컨트롤러입니다.")
 public class TestController {
 
     private final TestService teatservice;
 
     //db와 상호작용 필요없는 일반 api
     @GetMapping("/hello")
-    @ApiOperation(value = "헬로우 월드", notes = "헬로우 월드!!!!") //notes는 안적어도 상관없어요
+    @ApiOperation(value = "hello world 출력", notes = "hello world 반환") //notes는 안적어도 상관없어요
     public String Hello(){
         return "hello world";
     }
@@ -32,7 +32,7 @@ public class TestController {
 
     //jpa가 기본적으로 제공하는 함수를 이용해 db와 상호작용
     @GetMapping("/testDefault")
-    @ApiOperation(value = "jpa 기본 동작 확인", notes = "테스트 테이블의 칼럼 수를 반환한다.")
+    @ApiOperation(value = "jpa 기본 동작 확인", notes = "테스트 테이블의 칼럼 수를 반환")
     public long getCount(){
         return teatservice.getCount();
     }
@@ -40,7 +40,7 @@ public class TestController {
 
     //jpa가 기본적으로 제공하지 않지만, 비교적 간단한 쿼리문인 경우
     @GetMapping("/testCustom")
-    @ApiOperation(value = "jpa 동작 확인", notes = "이름에 '안녕'을 포함하는 칼럼 리스트를 반환")
+    @ApiOperation(value = "jpa dto 동작 확인", notes = "이름에 '안녕'을 포함하는 칼럼 리스트를 반환")
     public List<TestDto> getTest(){
         return teatservice.getCustom("안녕");
     }
