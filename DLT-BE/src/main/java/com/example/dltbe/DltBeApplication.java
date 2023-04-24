2afab0e6ec00fcfff7221c8c159cf1dc7f9444d1
Chaeeun Lee <chaen511@naver.com>
2023-04-24 13:28:18 +0900

    [FEAT] create project

---
+++ DLT-BE/src/main/java/com/example/dltbe/DltBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.dltbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DltBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DltBeApplication.class, args);
+	}
+
+}
