156cedb23ead006facad71f9692126dda51f5b90
김부경 <sa01023@naver.com>
2023-05-06 18:54:20 +0900

    [FIX]에러수정

--- backend/src/main/java/com/dopamines/backend/plan/controller/PlanController.java
+++ backend/src/main/java/com/dopamines/backend/plan/controller/PlanController.java
@@ -10,7 +10,6 @@ import com.dopamines.backend.plan.repository.PlanRepository;
 import com.dopamines.backend.plan.service.ParticipantService;
 import com.dopamines.backend.plan.service.PlanService;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,7 +20,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
