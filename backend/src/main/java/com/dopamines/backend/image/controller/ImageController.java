fe98a185ba4d96375c9de2ab4d1f2dfe5436c4bb
Chaeeun Lee <chaen511@naver.com>
2023-05-08 17:25:51 +0900

    [FEAT] editProfile

--- backend/src/main/java/com/dopamines/backend/image/controller/ImageController.java
+++ backend/src/main/java/com/dopamines/backend/image/controller/ImageController.java
@@ -1,32 +1,37 @@
 package com.dopamines.backend.image.controller;
 
 
+import com.dopamines.backend.account.entity.Account;
+import com.dopamines.backend.account.repository.AccountRepository;
 import com.dopamines.backend.image.service.ImageService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
+
+@Slf4j
 @RestController
 @RequestMapping("/image")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @Api(value = "test", description = "파일을 업로드 합니다.")
 public class ImageController {
+    private final ImageService imageService;
 
-    @Autowired
-    private final ImageService service;
+    @PutMapping("/profile")
+    @ApiOperation(value = "프로필 사진 바꾸기", notes = "프로필 사진을 MultipartFile 로 업로드하고 Account entity를 반환합니다.")
+    public ResponseEntity<Optional<Account>> editProfile(HttpServletRequest request, @RequestParam MultipartFile multipartFile) throws Exception  {
 
+        String email = request.getRemoteUser();
 
-    @PutMapping("/upload")
-    @ApiOperation(value = "파일 업로드", notes = "파일을 업로드하고 img url을 가져옵니다. 폴더명은 상황에 맞게..")
-    public ResponseEntity<Object> uploadAndGetLink(MultipartFile multipartFile, String folderName) throws Exception  {
-        String url=service.saveFile(multipartFile,folderName);
-        return new ResponseEntity<Object>(HttpStatus.OK);
+        return ResponseEntity.ok(imageService.editProfile(email, multipartFile));
     }
-
 }
