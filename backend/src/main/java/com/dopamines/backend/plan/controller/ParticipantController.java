520bd7da190d2448b0295fc8fc24066e843f5f41
kimbugyeong <sa01023@naver.com>
2023-05-11 14:43:01 +0900

    [FIX]swagger설명

--- backend/src/main/java/com/dopamines/backend/plan/controller/ParticipantController.java
+++ backend/src/main/java/com/dopamines/backend/plan/controller/ParticipantController.java
@@ -23,8 +23,8 @@ public class ParticipantController {
     private final ParticipantService participantService;
 
     @PostMapping("/registerGetMoney")
-    @Operation(summary = "게임에서 획득한 지각비 정보를 입력하는 api 입니다.", description = "게임이 끝난후 게임 결과에 따라 planId와 getMoney, balance를 입력하여 각 참가자의 거래 금액을 업데이트합니다." +
-            "getMoney는 일찍 온 사람, 지각자가 게임에서 획득한 금액을 입력합니다. 지각자가 획득한 금액은 지각비에서 제외됩니다." +
+    @Operation(summary = "게임에서 획득한 지각비 정보를 입력하는 api 입니다.", description = "게임이 끝난후 게임 결과에 따라 planId와 getMoney, balance를 입력하여 각 참가자의 거래 금액을 업데이트합니다.<br>" +
+            "getMoney는 일찍 온 사람, 지각자가 게임에서 획득한 금액을 입력합니다. 지각자가 획득한 금액은 지각비에서 제외됩니다.<br>" +
             "balance는 총 지각비 중 참가자들이 획득하지 못 한 남은 잔액을 입력합니다. 마지막 지각자가 도착하여 못 찾은 돈은 전체 참가자가 N빵하여 획득하게 됩니다.")
     public ResponseEntity<GameResultMoneyDto> registerGetMoney(
             HttpServletRequest request,
