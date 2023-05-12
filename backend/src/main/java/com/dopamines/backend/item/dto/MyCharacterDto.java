619caace7ccb457d06375d1915c2d117ecf6b3c5
Chaeeun Lee <chaen511@naver.com>
2023-05-12 17:52:01 +0900

    [FEAT]MyCharacterDto

--- backend/src/main/java/com/dopamines/backend/item/dto/MyCharacterDto.java
+++ backend/src/main/java/com/dopamines/backend/item/dto/MyCharacterDto.java
@@ -1,12 +1,13 @@
 package com.dopamines.backend.item.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.HashMap;
+
 @Getter
 @Setter
-public class ItemDto {
-    private int code;
-    private int price;
-
+@Builder
+public class MyCharacterDto {
 }
