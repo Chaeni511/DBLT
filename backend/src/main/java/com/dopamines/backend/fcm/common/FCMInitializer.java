5bc9a867b44191589582b7a96c8ac29acc8315a9
kimbugyeong <sa01023@naver.com>
2023-05-17 01:49:23 +0900

    [FEAT]SDK 초기화

--- backend/src/main/java/com/dopamines/backend/fcm/common/FCMInitializer.java
+++ backend/src/main/java/com/dopamines/backend/fcm/common/FCMInitializer.java
@@ -4,17 +4,39 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 
 @Slf4j
 @Component
 // 어플리케이션이 시작될 때 비공개 키 파일의 인증정보를 이용해 FirebaseApp을 초기화하도록 FCMInitializer 구현
 public class FCMInitializer {
+
+    @Value("${fcm.certification}")
+    private String googleApplicationCredentials;
+
+    @PostConstruct
+    public void initialize() throws IOException {
+        ClassPathResource resource = new ClassPathResource(googleApplicationCredentials);
+
+        try (InputStream is = resource.getInputStream()) {
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(is))
+                    .build();
+
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+                log.info("FirebaseApp initialization complete");
+            }
+        }
+    }
 
 //    @PostConstruct
 //    public void initializer() throws IOException {
@@ -28,5 +50,6 @@ public class FCMInitializer {
 //
 //        FirebaseApp.initializeApp(options);
 //    }
+
 
 }
