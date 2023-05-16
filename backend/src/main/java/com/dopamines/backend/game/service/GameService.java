8828c3c16cec01a3753b0e71cfe608ac69da2b33
Bigdog93 <ljc9393@nate.com>
2023-05-16 22:46:02 +0900

    [FIX] int to Integer

--- backend/src/main/java/com/dopamines/backend/game/service/GameService.java
+++ backend/src/main/java/com/dopamines/backend/game/service/GameService.java
@@ -5,7 +5,7 @@ import com.dopamines.backend.game.dto.GameResponseDto;
 public interface GameService {
 
     GameResponseDto enterGame(String email, Long planId);
-    int updateMoney(String email, long planId, int money);
+    int updateMoney(String email, Long planId, Integer money);
     void exitGame(String email, Long planId, int transactionMoney);
 
 }
