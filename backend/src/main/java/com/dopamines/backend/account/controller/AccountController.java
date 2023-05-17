41466131c45989fd0330356eaa0258282892e626
Chaeeun Lee <chaen511@naver.com>
2023-05-17 13:32:54 +0900

    [FIX] pull merge

--- backend/src/main/java/com/dopamines/backend/account/controller/AccountController.java
+++ backend/src/main/java/com/dopamines/backend/account/controller/AccountController.java
@@ -64,9 +64,10 @@ public class AccountController {
             return ResponseEntity.badRequest().build();
         } else {
             log.info("account.get().getAccountId(): " + account.get().getAccountId());
+            log.info("account.get().getNickname(): " + account.get().getNickname());
             log.info("account.get().getProfile(): " + account.get().getProfile());
 
-            UserInfoResponseDto userInfoResponseDto = new UserInfoResponseDto(account.get().getAccountId(), account.get().getProfile());
+            UserInfoResponseDto userInfoResponseDto = new UserInfoResponseDto(account.get().getAccountId(), account.get().getNickname(), account.get().getProfile());
             return ResponseEntity.ok(userInfoResponseDto);
         }
 
