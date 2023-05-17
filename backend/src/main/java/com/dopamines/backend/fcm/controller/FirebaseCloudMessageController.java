f2483f90d164bb635779148e767d5aeb9a33d7b9
kimbugyeong <sa01023@naver.com>
2023-05-17 15:32:19 +0900

    [FEAT]주제구독fcm

--- backend/src/main/java/com/dopamines/backend/fcm/controller/FirebaseCloudMessageController.java
+++ backend/src/main/java/com/dopamines/backend/fcm/controller/FirebaseCloudMessageController.java
@@ -1,9 +1,10 @@
 package com.dopamines.backend.fcm.controller;
 
 import com.dopamines.backend.fcm.dto.GroupTokenListDto;
+import com.dopamines.backend.fcm.dto.TopicRequestDto;
 import com.dopamines.backend.fcm.dto.RequestDto;
 import com.dopamines.backend.fcm.dto.TokenDto;
-import com.dopamines.backend.fcm.service.FCMService;
+import com.dopamines.backend.fcm.service.FirebaseCloudMessageService;
 import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
@@ -22,9 +23,9 @@ import java.io.IOException;
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @Api(value = "fcm", description = "firebaseCloudMessage를 관리하는 컨트롤러입니다.")
-public class FCMController {
+public class FirebaseCloudMessageController {
 
-    private final FCMService fcmService;
+    private final FirebaseCloudMessageService fcmService;
 
 
     @PostMapping("/register")
@@ -99,6 +100,29 @@ public class FCMController {
             log.error("API 호출 중 예외 발생: {}", e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
+    }
+
+
+    @PostMapping("/topicPush")
+    @Operation(summary = " 주제를 구독한 클라이언트에게 fcm 알림을 보내는 api 입니다.", description = "topic, title, body, planId, type를 작성하여 보내면 firebase 서버에 메세지 푸시를 요청하여 주제를 구독한 클라이언트에게 메세지를 푸시합니다")
+    public ResponseEntity<Void> pushTopicMessage(@RequestBody TopicRequestDto topicRequestDto) throws IOException {
+        // 1. targetToken, title, body를 작성하여 서버에 보낸다.
+        // 2. 서버는 firebase 서버에 메세지 푸시를 요청한다.
+        // 3. 토큰이 확인되면 firebase서버는 클라이언트에게 메세지를 푸시한다.
+
+        log.info("Topic: " + topicRequestDto.getTopic() + " Title: "
+                +topicRequestDto.getTitle() + " Body: " + topicRequestDto.getBody());
+
+        fcmService.sendTopicMessageTo(
+                topicRequestDto.getTopic(),
+                topicRequestDto.getTitle(),
+                topicRequestDto.getBody(),
+                topicRequestDto.getPlanId(),
+                topicRequestDto.getType()
+        );
+
+
+        return ResponseEntity.ok().build();
     }
 
 
--- backend/src/main/java/com/dopamines/backend/fcm/repository/FirebaseCloudMessageRepository.java
+++ backend/src/main/java/com/dopamines/backend/fcm/repository/FirebaseCloudMessageRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface FCMRepository extends JpaRepository<FCM, Long> {
+public interface FirebaseCloudMessageRepository extends JpaRepository<FCM, Long> {
 
     Optional<FCM> findByAccount(Account account);
 }
