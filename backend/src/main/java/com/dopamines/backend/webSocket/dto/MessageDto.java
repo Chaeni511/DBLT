af72d1c93c3c20d560d4037800facddced19d6d4
kimbugyeong <sa01023@naver.com>
2023-05-18 14:51:44 +0900

    [FIX]nickname웹소켓에추가

--- backend/src/main/java/com/dopamines/backend/webSocket/dto/MessageDto.java
+++ backend/src/main/java/com/dopamines/backend/webSocket/dto/MessageDto.java
@@ -15,6 +15,7 @@ public class MessageDto {
     private MessageType type; // 메시지 타입
     private String roomId; // 약속 방 번호
     private String sender; // 움직임을 보낸 사람 accountId
+    private String nickname;
     private String profile; // 프로필 이미지 사진
     private String message; // 메세지
     private String latitude; // 위도
